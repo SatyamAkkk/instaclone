ce961140d2e17681eadcee7cafcae4c9857a19f1
Satyam Akbari <satyamakbari473@gmail.com>
2025-11-04 14:56:11 +0530

    Fixed DB connection and server start

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,12 +1,29 @@
-import mongoose from "mongoose"
+// import mongoose from "mongoose"
 
-const connectDB = async ()=>{
-  try{
-    await mongoose.connect(process.env.MONGO_URL)
-    console.log("Mongodb Connected Successfully")
-  }catch(error){
-    console.log(error)
+// const connectDB = async ()=>{
+//   try{
+//     await mongoose.connect(process.env.MONGO_URL)
+//     console.log("Mongodb Connected Successfully")
+//   }catch(error){
+//     console.log(error)
+//   }
+// }
+
+// export default connectDB;
+
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected');
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
   }
-}
+};
 
 export default connectDB;
